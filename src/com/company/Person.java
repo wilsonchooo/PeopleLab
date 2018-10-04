ba8c119d4ffefa3a03a626eec99a1e4f4c10cffa
BT_1N3_30 <jhan3537@bths.edu>
2018-10-04 11:02:08 -0400

    did things

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,5 +1,28 @@
 package com.company;
 
 public abstract class Person {
+    // Fields
+    private String firstName;
+    private String familyName;
 
+    // Constructor
+    public Person(String firstName, String familyName) {
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+
+    public abstract String getFirstName();
+    public abstract String getFamilyName();
+    public abstract boolean equals(Person p);
 }
+
+
+/*
+Fields:
+firstName, familyName
+Methods:
+Constructor – must initialize fields
+getFirstName() - returns first name
+getFamilyName() - returns family name
+equals(Person p) – returns true if first and family name are equal
+ */
