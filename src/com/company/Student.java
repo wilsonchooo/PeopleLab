54f16ac7453c99282a30bfb288889b6b2690a10d
KarwaiKang <kkang9093@bths.edu>
2018-10-04 11:20:42 -0400

    Create Student.java

---
+++ src/com/company/Student.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Student extends Person {
+
+    // Fields
+
+    private double GPA;
+    private int grade;
+    private String major;
+
+    // Constructor
+
+    public Student(String firstName, String familyName, double GPA, int grade, String major) {
+        super(firstName, familyName);
+        this.GPA = GPA;
+        this.grade = grade;
+        this.major = major;
+    }
+
+    // Methods
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String toString() {
+        return getFamilyName() + ", " + getFirstName();
+    }
+}
