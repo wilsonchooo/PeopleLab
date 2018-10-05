7707c51bd2b2ed54f148392e0476eb57bae7fa00
KarwaiKang <kkang9093@bths.edu>
2018-10-05 11:03:09 -0400

    Update JavaDocs

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -1,21 +1,41 @@
 package com.company;
 
 public abstract class Teacher extends Person {
-    //Fields
+    // Fields
     private String Subject;
     private String Title;
 
-    //Constructor
+    // Constructor
     public Teacher(String firstName, String familyName, String Subject, String Title)
     {
         super(firstName, familyName);
         this.Subject = Subject;
         this.Title = Title;
     }
+
+    // Methods
+
+    // Getters
     public String getSubject()
     {
         return Subject;
     }
+    public String getTitle() {
+        return Title;
+    }
+
+    // Setters
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    /**
+     * @return "Title. FamilyName"
+     */
+
     public String toString()
     {
         return Title + ". " + getFamilyName();
