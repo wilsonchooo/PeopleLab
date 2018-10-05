7d304222b546fa252356af1accc0c1d5b6eedc37
KarwaiKang <kkang9093@bths.edu>
2018-10-05 11:37:11 -0400

    Removed garbage

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -5,7 +5,7 @@ public class Runner {
     private static String[] firstNames = new String[]{"Abir","Madhi","Parker","Nusrat","Vincent","Jacob","Christina","Andrew","Zeng","Wilson","Mead","Aaron","Karwai","James","Eugene","Bryant","Sabrina","Lack","Kevin","Azarul","Nicholas","Arifur","Rachel","Chen","Kyle","Daniyah","Catelen","Edmund","Jackie","Aaron","Darren","Angela","Jesse"};
     private static String[] familyNames = new String[]{"Ahmed","Ahmed","Ahmed","Alam","Bond-Moore","Cedar","Chau","Chen","Chen","Cho","Gyawu","Jack","Kang","Ko","Korsunsky","Lay","Li","Liu","Liu","Mohammed","Porzio","Rahman","Shek","Stanilovsky","Tam","Wong","Wu","Wu","Wu","Yafet","Yau","Zheng","Han"};
 
-    // Method
+    // Methods
     public static void main(String[] args) {
         Teacher ourTeacher = new Teacher("Nathan","Folwell","AP Java","Mr");
         Student[] ourStudents = new Student[32];
@@ -18,17 +18,5 @@ public class Runner {
     }
     public static Student randomStudent() {
         return new Student(firstNames[(int)(Math.random() * 32)],familyNames[(int)(Math.random() * 32)],(Math.random() * 4),12,"Software Engineering");
-    };
+    }
 }
-
-
-/*
- Fields:
- Static String[] firstNames; //a pre-made list of first names
- Static String[] familyNames; //a pre-made list of family names
- Method (Only the main method):
- public static void main(String[] args)
- - Declare and initialize a teacher, an array of students, and a classroom
- - Print Class, and print class Average.
- randomStudent() - builds a random student with a random first name, last name, and GPA
- */
