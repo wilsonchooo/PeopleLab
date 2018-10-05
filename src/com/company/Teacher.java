132987fc0dfe0a64211997e5fab0651a690cd22a
BT_1N3_29 <dccd>
2018-10-05 10:44:37 -0400

    stuff

---
+++ src/com/company/Teacher.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public abstract class Teacher extends Person {
+    //Field
+    public String Subject;
+    public String Title;
+
+    //Constructor
+    public abstract String getFamilyName();
+    public Teacher(String Subject, String Title)
+    {
+        super("Teacher", "familyName");
+        this.Subject = Subject;
+        this.Title = Title;
+    }
+    public String getSubject()
+    {
+        return Subject;
+    }
+    public String toString()
+    {
+        return Title + ". " + familyName;
+    }
+}
+
