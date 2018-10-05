b783b0d85e345d2ddad5e28d8848ee08347eb25a
KarwaiKang <kkang9093@bths.edu>
2018-10-05 10:53:06 -0400

    Generate getters and setters Update JavaDocs

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -1,11 +1,11 @@
 package com.company;
 
 public abstract class Teacher extends Person {
-    //Fields
+    // Fields
     public String Subject;
     public String Title;
 
-    //Constructor
+    // Constructor
     public abstract String getFamilyName();
     public Teacher(String Subject, String Title)
     {
@@ -13,13 +13,33 @@ public abstract class Teacher extends Person {
         this.Subject = Subject;
         this.Title = Title;
     }
+
+    // Getters
     public String getSubject()
     {
         return Subject;
     }
+    public String getTitle() {
+        return Title;
+    }
+
+    // Setters
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    /**
+     * @return "Title. FamilyName"
+     */
+
     public String toString()
     {
         return Title + ". " + familyName;
     }
+
+
 }
 
